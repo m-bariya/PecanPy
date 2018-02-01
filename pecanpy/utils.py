54bad05de784780ba7eaaf6a6cd298cf9222aeff
davidrpugh <david.pugh@kapsarc.org>
2018-02-01 11:23:30 +0300

    Made changes requested by grant.

--- pecanpy/utils.py
+++ pecanpy/utils.py
@@ -1,24 +1,14 @@
+"""
+Module contining utility functions.
+
+@author davidrpugh
+
+"""
 import sqlalchemy
 
 
-def create_local_engine(user_name, password):
-    """Create an SQL engine given KAPSARC credentials."""
-    host = "helium-63.test.kapsarc"
-    port = 5432
-    db = "pecan_raw"
-    return _create_engine(user_name, password, host, port, db)
-
-
-def create_remote_engine(user_name, password):
-    """Create a PostgreSQL engine given Pecan Street Dataport credentials."""
-    host = "dataport.cloud"
-    port = 5434
-    db = "postgres"
-    return _create_engine(user_name, password, host, port, db)
-
-
-def _create_engine(user_name, password, host, port, db):
-    """Create an PostgreSQL engine."""
+def create_engine(user_name, password, host, port, db):
+    """Create a PostgreSQL engine."""
     url = "postgresql://{}:{}@{}:{}/{}".format(user_name, password, host, port, db)
     engine = sqlalchemy.create_engine(url)
     return engine
