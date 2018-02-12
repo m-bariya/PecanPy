30ffa295cd05378713038d28832b8ee0c616805e
J. Andrew howe <ahowe42@gmail.com>
2018-02-12 15:16:01 +0300

    Update electricity_egauge_api.py
    Added a new function `execute_generic_query`, which allows the user to execute an arbitrary SQL Select query against a Pecan Street Dataport database.

--- pecanpy/electricity_egauge_api.py
+++ pecanpy/electricity_egauge_api.py
@@ -13,6 +13,49 @@ import pandas as pd
 from pandas.api import types
 import sqlalchemy
 
+def execute_generic_query(con: sqlalchemy.engine.Connectable,
+                         SQLstr = None,
+                         SQLfile = None) -> pd.DataFrame:
+    """
+    Execute arbitrary SQL Select query against a database, returning results
+    in a pandas DataFrame. No data manipulation or munging is performed. Either
+    SQLstr or SQLfile should be not None. Either way, the first word in the SQL
+    query should be "SELECT".
+
+    Parameters:
+    -----------
+    con = `sqlalchemy.engine.Connectable`
+        An object which supports execution of SQL constructs. Currently there
+        are two implementations: `sqlalchemy.engine.Connection` and
+        `sqlalchemy.engine.Engine`.    
+    SQLstr = `str`
+        string holding the select query to execute
+    SQLfile = `str`
+        full path to text file holding the select query to execute
+
+    Returns:
+    --------
+    pandas dataframe holding query results
+    """
+    
+    # input is either through a SQL string or a file with SQL code
+    if (SQLstr is None) and (SQLfile is None):
+      raise TypeError('Please pass either the SQLstr or the SQLfile argument!')
+    elif (SQLstr is not None):
+      SQL = SQLstr
+    elif (SQLfile is not None):
+      SQL = open(SQLfile,'rt').read()
+    else:
+      return None # should never happend
+    
+    # enforce that this must be a select query
+    try:
+      SQL.upper().index('SELECT',0)
+    except ValueError as e:
+      raise ValueError('SQL statement must start with "SELECT"!')
+    
+    return pd.read_sql_query(SQL, con)
+  
 
 def read_electricity_egauge_query(con: sqlalchemy.engine.Connectable,
                                   schema: str,
