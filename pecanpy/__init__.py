e3315d85e4ec76e59054e631faa9ba263e00b611
davidrpugh <david.pugh@kapsarc.org>
2018-02-12 08:56:50 +0300

    Fixed imports.

--- pecanpy/__init__.py
+++ pecanpy/__init__.py
@@ -4,7 +4,7 @@ Objects imported here will live in the `pecanpy` namespace.
 @author : davidrpugh
 
 """
-from . api import *
-from . electricity_egauge import *
-from . surveys import *
+from . gas_water_api import *
+from . electricity_egauge_api import *
+from . surveys_api import *
 from . utils import *
